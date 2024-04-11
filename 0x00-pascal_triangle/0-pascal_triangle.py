ee5c0807d6ca94e79c4062fc1a11a2c225aed621
NezhaviAbderrahmane <cszamasu@gmail.com>
2024-04-11 17:21:02 +0000

    A

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,37 +1,44 @@
 #!/usr/bin/python3
 """AAAA"""
-import math
 
-def _factorial(n):
-    """AAAA"""
-    r = 1
+def main():
+    """looooool"""
+    import math
 
-    for _ in range(1, n + 1):
-        r *= _
+    def _factorial(n):
+        """AAAA"""
+        r = 1
 
-    return r
+        for _ in range(1, n + 1):
+            r *= _
+
+        return r
 
 
-def pascal_triangle(n):
-    """AAAAAA"""
-    if n <= 0:
-        return []
+    def pascal_triangle(n):
+        """AAAAAA"""
+        if n <= 0:
+            return []
 
-    arr = []
+        arr = []
 
-    for i in range(n):
-        _r = []
-        for j in range(n):
-            _r.append(math.floor(_factorial(i) / (_factorial(j) * _factorial(i - j))))
+        for i in range(n):
+            _r = []
+            for j in range(n):
+                _r.append(math.floor(_factorial(i) / (_factorial(j) * _factorial(i - j))))
         
-        for _ in range(n):
-            if 0 in _r:
-                _r.remove(0)
+            for _ in range(n):
+                if 0 in _r:
+                    _r.remove(0)
         
     
-        arr.append(_r)
+            arr.append(_r)
     
-    if n > 1:
-        arr[0].pop()
+        if n > 1:
+            arr[0].pop()
         
-    return arr
+        return arr
+
+
+if __name__ == "__main__":
+    main() 
