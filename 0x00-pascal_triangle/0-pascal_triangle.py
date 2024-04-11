8e4e48df473d8e214bdc0924911ba4bf002e9e06
NezhaviAbderrahmane <cszamasu@gmail.com>
2024-04-11 16:44:04 +0000

    A

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,30 @@
+import math
+
+def _factorial(n):
+    r = 1
+
+    for _ in range(1, n + 1):
+        r *= _
+
+    return r
+
+
+def pascal_triangle(n):
+    arr = []
+
+    for i in range(n):
+        _r = []
+        for j in range(n):
+            _r.append(math.floor(_factorial(i) / (_factorial(j) * _factorial(i - j))))
+        
+        for _ in range(n):
+            if 0 in _r:
+                _r.remove(0)
+        
+    
+        arr.append(_r)
+    
+    if n > 1:
+        arr[0].pop()
+        
+    return arr
