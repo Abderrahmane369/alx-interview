f2ba76ceb9871246ea0a939c4be376c07012218c
Abderrahmane <cszamasu@gmail.com>
2024-04-23 19:28:36 +0100

    a

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,16 +4,16 @@
 
 def minOperations(n):
     """Calculate the minimum number of operations."""
-    if n <= 1:
+    if n <= 0:
         return 0
 
-    operations = 0
     p = 2
+    minOp = 0
 
     while n > 1:
         while n % p == 0:
-            operations += p
+            minOp += p
             n /= p
         p += 1
 
-    return operations
+    return minOp
