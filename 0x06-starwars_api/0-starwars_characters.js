b0f83ea970ac0d7f59f37703af4aea75cf7cd073
Abderrahmane <Abderrahmane@369@gmail.com>
2024-06-23 08:42:48 -0700

    A

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -8,12 +8,18 @@ const argv = process.argv;
 request(`${url}films/${argv[2]}`, {json: true}, (e, r, b) => {
 	if (e || r.statusCode !== 200) return;
 
-	b.characters.forEach((v, _) => {
-		request(v, {json:true}, (e_, r_, b_) => {
+	function recurFetch(i, chrtrs) {
+		if (i >= chrtrs.length) return;
+
+		request(chrtrs[i], {json:true}, (e_, r_, b_) => {
 			if (e_ || r_.statusCode !== 200) return;
 
 			console.log(b_.name);
 		})
-	});
+
+		recurFetch(i + 1, chrtrs);
+	}
+
+	recurFetch(0, b.characters);
 });
 
