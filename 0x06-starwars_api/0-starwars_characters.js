d24fa4ba1f76844f265539fe9baa53d662ba7412
Abderrahmane <Abderrahmane@369@gmail.com>
2024-06-23 08:11:36 -0700

    A

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,17 @@
+const request = require("request");
+const url = "https://swapi-api.alx-tools.com/api/";
+const argv = process.argv;
+
+
+request(`${url}films/${argv[2]}`, {json: true}, (e, r, b) => {
+	if (e || r.statusCode !== 200) return;
+
+	b.characters.forEach((v, _) => {
+		request(v, {json:true}, (e_, r_, b_) => {
+			if (e_ || r_.statusCode !== 200) return;
+
+			console.log(b_.name);
+		})
+	});
+});
+
